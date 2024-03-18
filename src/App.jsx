6ecb468ff0394fd309c10aa53fcc9013b3b38e87
Naiemjoy1 <naiemjoy1@gmail.com>
2024-03-18 22:10:15 +0600

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import NavBar from './Components/NavBar/NavBar'
+// import DaisyNav from './Components/DaisyNav/DaisyNav'
 
 function App() {
 
   return (
     <>
-      
+      {/* <DaisyNav></DaisyNav> */}
+      <NavBar></NavBar>
       <h1 className=' text-white bg-green-600 text-center'>Vite + React</h1>
       
     </>
