3328712d5a2940f02228df394e6db7720a2cf4ef
Naiemjoy1 <naiemjoy1@gmail.com>
2024-03-18 21:35:55 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1 className=' text-white bg-green-600 text-center'>Vite + React</h1>
+      
+    </>
+  )
+}
+
+export default App
