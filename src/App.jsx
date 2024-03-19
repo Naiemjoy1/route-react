91bb958475289963220a2c12d689b6b45c1ade80
Naiemjoy1 <naiemjoy1@gmail.com>
2024-03-19 09:22:47 +0600

    navbar button

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css'
 import NavBar from './Components/NavBar/NavBar'
-// import DaisyNav from './Components/DaisyNav/DaisyNav'
+import DaisyNav from './Components/DaisyNav/DaisyNav'
 
 function App() {
 
   return (
     <>
-      {/* <DaisyNav></DaisyNav> */}
       <NavBar></NavBar>
+      <DaisyNav></DaisyNav>
       <h1 className=' text-white bg-green-600 text-center'>Vite + React</h1>
       
     </>
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import Link from "../Link/Link";
+import { RiMenu2Fill } from "react-icons/ri";
+import { MdClose } from "react-icons/md";
 
 const NavBar = () => {
+    const [open, setOpen] = useState(false)
+
     const routes =[
         { id: 1, name: 'Home', path: '/' },
         { id: 2, name: 'About', path: '/about' },
@@ -11,6 +16,14 @@ const NavBar = () => {
 
     return (
         <nav>
+            <div className="md:hidden text-xl" onClick={() => setOpen(!open)}>
+                {
+                    open === true ? 
+                    <MdClose></MdClose> 
+                    : <RiMenu2Fill className=" "></RiMenu2Fill>
+                }
+                
+            </div>
             <ul className="md:flex gap-6">
             {
                 routes.map(route => <Link key={route.id} route={route}></Link>)
